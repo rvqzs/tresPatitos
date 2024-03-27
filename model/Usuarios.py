2197289e146892cbeb2f1d9db2dd6e09cb001070
rviquez9 <146051750+rviquez9@users.noreply.github.com>
2024-03-26 20:29:48 -0600

    UI Usuarios
    Se introdujo UI Usuarios CRUD

---
+++ model/Usuarios.py
@@ -0,0 +1,70 @@
+import pymongo
+
+class Usuarios:
+    def __init__(self, id, nombreUsuario, email):
+        self.id = id
+        self.nombreUsuario = nombreUsuario
+        self.email = email
+
+        #self.telefono = telefono
+        #self.direccion= direccion
+        #self.puesto=puesto
+        #self.ingreso=ingreso
+        #self.jefatura=jefatura
+    
+    def guardar(self):
+        #abrir la conxion mediante un objeto cliente
+        usuarios=pymongo.MongoClient("mongodb://localhost:27017")
+        bd=usuarios["Empresa"]
+        try:
+            #definir la tabla a utilizar
+            tbl=bd["Usuarios"]
+            #crear diccionario
+            doc={"_id":self.id,"Usuario":self.nombreUsuario,"email":self.email}
+            #insertar en la tabla
+            tbl.insert_one(doc)
+            estado=1
+        except Exception:
+            print("error al guardar")
+            estado=0
+        finally:
+            usuarios.close
+        return estado
+    
+    def actualizar(self):
+        #abrir la conxion mediante un objeto cliente
+        usuarios=pymongo.MongoClient("mongodb://localhost:27017")
+        bd=usuarios["Empresa"]
+        try:
+            #definir la tabla a utilizar
+            tbl=bd["Usuarios"]
+            #filtro sirve para ver que quiero modificar
+            filtro={"_id":self.id}
+            #crear diccionario
+            doc={"$set":{"Usuario":self.nombreUsuario,"email":self.email}}
+            #insertar en la tabla
+            tbl.update_one(filtro,doc)
+            estado=1
+        except Exception:
+            print("error al guardar")
+            estado=0
+        finally:
+            usuarios.close
+        return estado
+    
+    def eliminar(self):
+        usuarios=pymongo.MongoClient("mongodb://localhost:27017")
+        bd=usuarios["Empresa"]
+        try:
+            #definir la tabla a utilizar
+            tbl=bd["Usuarios"]
+            #filtro sirve para ver que quiero modificar
+            filtro={"_id":self.id}
+            tbl.delete_one(filtro)
+            estado=1
+        except Exception:
+            print("error al Eliminar")
+            estado=0
+        finally:
+            usuarios.close
+        return estado
