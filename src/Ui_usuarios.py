d3bf0fb9218dffca029dc8858ccdca3110ca8a36
rviquez <146051750+rviquez9@users.noreply.github.com>
2024-03-30 10:14:10 -0600

    Update UI Usuarios

--- src/Ui_usuarios.py
+++ src/Ui_usuarios.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\rviqu\OneDrive - Universidad Internacional de las Américas (UIA)\UIA\2024\I\Programación II\Proyecto Final\tresPatitos\tresPatitosUIA\src\Usuarios.ui'
+# Form implementation generated from reading ui file 'c:\Users\rviqu\OneDrive - Universidad Internacional de las Américas (UIA)\UIA\2024\I\Programación II\Proyecto Final\tresPatitosUIA\src\Usuarios.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
