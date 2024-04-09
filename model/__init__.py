0f646f8c1a38a870d85cdde84f035d2e4a09b484
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-04-09 01:03:42 -0600

    Create __init__.py
    test

---
+++ model/__init__.py
@@ -0,0 +1,7 @@
+from model.usuarios import Usuarios
+from model.empleados import Empleados
+from model.departamentos import Departamentos
+from model.bienes import Bienes
+from model.asignacion import AsignarBienes
+from model.desligar import DesligarBienes
+from model.reporteBienesAsignados import ReporteBienesAsignados
